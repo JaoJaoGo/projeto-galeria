a2bdabd799e15b2e1f2fdd5cf55cfbd57e4d6026
João V G Carrijo <jvgcarrijo@outlook.com>
2024-06-04 15:05:40 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Dependencias
+import 'jquery';
+import 'bootstrap';
+
+// Arquivos SASS
+import './scss/index.scss';
+
+// Meus arquivos JS
+import './js/core/includes';
+import './js/plugins/cityButtons';
